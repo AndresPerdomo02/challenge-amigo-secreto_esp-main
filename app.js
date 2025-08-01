7d271676ea4ae29b0366f981b8a29e4d37e80cb3
AndresPerdomo <caperdomo92@gmail.com>
2025-08-01 17:12:02 -0500

    Se creo función mostrarAmigos

--- app.js
+++ app.js
@@ -9,13 +9,26 @@ function agregarAmigo() {
         alert('Por favor ingrese un nombre');
     }
     else {
-        amigos.push(amigo);
-        limpiarCaja();
-        return amigo;
+        amigos.push(amigo); // Se agrega un amigo a la lista 
+        mostrarAmigos(); // Se muestra la lista de amigos
+        limpiarCaja(); // Se limpia la caja
+        
     }
 }
 
 function limpiarCaja() {
     let valorCaja = document.querySelector('#amigo');
     valorCaja.value = '';
+}
+
+function mostrarAmigos() {
+    let listaHTML = document.getElementById("listaAmigos"); // Se declara la lista
+    lista.innerHTML = ""; // Se pone como vacía
+
+    for(let i = 0; i < amigos.length; i++) {
+        let listItem = document.createElement("li");
+        listItem.textContent = amigos[i];
+        listaHTML.appendChild(listItem);
+    }
+
 }
