3d5ad911c4ba135c33a3d0e94bad24e32c1c9287
AndresPerdomo <caperdomo92@gmail.com>
2025-08-01 16:24:02 -0500

    Se añadio la funcion agregarAmigo

---
+++ app.js
@@ -0,0 +1,23 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+
+    if (amigo == ''){
+        alert('Por favor ingrese un nombre');
+        return agregarAmigo();
+    }
+    else {
+        amigos.push(amigo);
+        limpiarCaja();
+        return amigo;
+    }
+}
+
+
+function limpiarCaja() {
+    let valorCaja = document.querySelector('amigo');
+    valorCaja.value = '';
+}
